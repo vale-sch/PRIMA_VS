c0e1e98bbb38622c873c1598e5b47d5c5a7ed8ca
vale-sch <franz.valentin.schmidberger@hs-furtwangen.de>
2021-03-23 17:56:38 +0100

    L01_FirstFudge(Mar23)

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -2,24 +2,21 @@
 var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var fCore = FudgeCore;
-    window.addEventListener("load", hndLoad);
-    function hndLoad(_event) {
-        const canvas = document.querySelector("canvas");
-        fCore.Debug.log(canvas);
-        let node = new fCore.Node("Quad");
-        let mesh = new fCore.MeshQuad();
-        let cmpMesh = new fCore.ComponentMesh(mesh);
-        node.addComponent(cmpMesh);
-        let mtrSolidWhite = new fCore.Material("SolidWhite", fCore.ShaderUniColor, new fCore.CoatColored(fCore.Color.CSS("WHITE")));
-        let cmpMaterial = new fCore.ComponentMaterial(mtrSolidWhite);
+    window.addEventListener("load", init);
+    function init(_event) {
+        let node = new fCore.Node("Test");
+        let canvas = document.querySelector("canvas");
+        let mesh = new fCore.MeshQuad("Quad");
+        node.addComponent(new fCore.ComponentMesh(mesh));
+        let material = new fCore.Material("FirstMaterial", fCore.ShaderUniColor, new fCore.CoatColored(new fCore.Color(1, 1, 1, 1)));
+        let cmpMaterial = new fCore.ComponentMaterial(material);
         node.addComponent(cmpMaterial);
         let cmpCamera = new fCore.ComponentCamera();
-        cmpCamera.mtxPivot.translateZ(2);
+        cmpCamera.mtxPivot.translateZ(3);
         cmpCamera.mtxPivot.rotateY(180);
-        L01_FirstFudge.viewport = new fCore.Viewport();
-        L01_FirstFudge.viewport.initialize("Viewport", node, cmpCamera, canvas);
-        fCore.Debug.log(L01_FirstFudge.viewport);
-        L01_FirstFudge.viewport.draw();
+        let viewport = new fCore.Viewport();
+        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
 //# sourceMappingURL=Test.js.map
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,31 +1,24 @@
 namespace L01_FirstFudge {
     import fCore = FudgeCore;
+    window.addEventListener("load", init);
 
-    window.addEventListener("load", hndLoad);
-    export let viewport: fCore.Viewport;
+    function init(_event: Event): void {
+        let node: fCore.Node = new fCore.Node("Test");
+        let canvas: HTMLCanvasElement = document.querySelector("canvas");
+        
+        let mesh: fCore.MeshQuad = new fCore.MeshQuad("Quad");
+        node.addComponent(new fCore.ComponentMesh(mesh));
 
-    function hndLoad(_event: Event): void {
-        const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        fCore.Debug.log(canvas);
-
-        let node: fCore.Node = new fCore.Node("Quad");
-
-        let mesh: fCore.MeshQuad = new fCore.MeshQuad();
-        let cmpMesh: fCore.ComponentMesh = new fCore.ComponentMesh(mesh);
-        node.addComponent(cmpMesh);
-
-        let mtrSolidWhite: fCore.Material = new fCore.Material("SolidWhite", fCore.ShaderUniColor, new fCore.CoatColored(fCore.Color.CSS("WHITE")));
-        let cmpMaterial: fCore.ComponentMaterial = new fCore.ComponentMaterial(mtrSolidWhite);
+        let material: fCore.Material = new fCore.Material("FirstMaterial", fCore.ShaderUniColor, new fCore.CoatColored(new fCore.Color(1, 1, 1, 1)));
+        let cmpMaterial: fCore.ComponentMaterial = new fCore.ComponentMaterial(material);        
         node.addComponent(cmpMaterial);
 
         let cmpCamera: fCore.ComponentCamera = new fCore.ComponentCamera();
-        cmpCamera.mtxPivot.translateZ(2);
+        cmpCamera.mtxPivot.translateZ(3);
         cmpCamera.mtxPivot.rotateY(180);
 
-        viewport = new fCore.Viewport();
+        let viewport: fCore.Viewport = new fCore.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
-        fCore.Debug.log(viewport);
-
         viewport.draw();
     }
 }
