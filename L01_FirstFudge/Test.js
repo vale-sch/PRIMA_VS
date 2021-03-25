b88b58a24a646a891ff6bb406d298898668e66b7
vale-sch <franz.valentin.schmidberger@hs-furtwangen.de>
2021-03-25 17:22:09 +0100

    First Update Methode

--- L01_FirstFudge/Test.js
+++ L01_FirstFudge/Test.js
@@ -3,9 +3,11 @@ var L01_FirstFudge;
 (function (L01_FirstFudge) {
     var fCore = FudgeCore;
     window.addEventListener("load", init);
+    let node = new fCore.Node("Test");
+    let viewport = new fCore.Viewport();
     function init(_event) {
-        let node = new fCore.Node("Test");
         let canvas = document.querySelector("canvas");
+        node.addComponent(new fCore.ComponentTransform());
         let mesh = new fCore.MeshQuad("Quad");
         node.addComponent(new fCore.ComponentMesh(mesh));
         let material = new fCore.Material("FirstMaterial", fCore.ShaderUniColor, new fCore.CoatColored(new fCore.Color(1, 1, 1, 1)));
@@ -14,8 +16,15 @@ var L01_FirstFudge;
         let cmpCamera = new fCore.ComponentCamera();
         cmpCamera.mtxPivot.translateZ(3);
         cmpCamera.mtxPivot.rotateY(180);
-        let viewport = new fCore.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+        fCore.Loop.start(fCore.LOOP_MODE.TIME_REAL, 60);
+        fCore.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+    }
+    function update(_event) {
+        let rotSpeed = 90 / 1000;
+        let deltaTime = fCore.Loop.timeFrameReal / 1000;
+        node.getComponent(fCore.ComponentMesh).mtxPivot.rotateZ(rotSpeed * deltaTime);
         viewport.draw();
     }
 })(L01_FirstFudge || (L01_FirstFudge = {}));
--- L01_FirstFudge/Test.ts
+++ L01_FirstFudge/Test.ts
@@ -1,12 +1,15 @@
 namespace L01_FirstFudge {
     import fCore = FudgeCore;
     window.addEventListener("load", init);
+    let node: fCore.Node = new fCore.Node("Test");
+    let viewport: fCore.Viewport = new fCore.Viewport();
 
     function init(_event: Event): void {
-        let node: fCore.Node = new fCore.Node("Test");
         let canvas: HTMLCanvasElement = document.querySelector("canvas");
-        
-        let mesh: fCore.MeshQuad = new fCore.MeshQuad("Quad");
+       
+        node.addComponent(new fCore.ComponentTransform());
+       
+        let mesh: fCore.Mesh = new fCore.MeshQuad("Quad");
         node.addComponent(new fCore.ComponentMesh(mesh));
 
         let material: fCore.Material = new fCore.Material("FirstMaterial", fCore.ShaderUniColor, new fCore.CoatColored(new fCore.Color(1, 1, 1, 1)));
@@ -17,8 +20,16 @@ namespace L01_FirstFudge {
         cmpCamera.mtxPivot.translateZ(3);
         cmpCamera.mtxPivot.rotateY(180);
 
-        let viewport: fCore.Viewport = new fCore.Viewport();
         viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.draw();
+
+        fCore.Loop.start(fCore.LOOP_MODE.TIME_REAL, 60);
+        fCore.Loop.addEventListener(fCore.EVENT.LOOP_FRAME, update);
+    }
+    function update(_event: Event): void {
+        let rotSpeed: number = 90 / 1000;
+        let deltaTime: number = fCore.Loop.timeFrameReal / 1000;
+        node.getComponent(fCore.ComponentMesh).mtxPivot.rotateZ(rotSpeed * deltaTime);
         viewport.draw();
     }
 }
