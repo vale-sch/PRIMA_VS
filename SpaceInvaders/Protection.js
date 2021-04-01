4ef3e53548485161c34f8ebbf39ffd31d1567901
vale-sch <vali.vogt@live.de>
2021-04-01 16:14:35 +0200

    L02 SpaceInvaders // Klassen hinzugefügt // init aufgeräumt

--- SpaceInvaders/Protection.js
+++ SpaceInvaders/Protection.js
@@ -6,10 +6,21 @@ var SpaceInvaders;
         constructor(_x) {
             super("Protection" + _x);
             let quads = new fCore.MeshQuad("Quads");
-            let cmpMaterialQuad = new fCore.ComponentMaterial(SpaceInvaders.materialGreen);
-            this.addComponent(new fCore.ComponentMesh(quads));
+            for (let i = 0; i < 5; i++) {
+                for (let j = 0; j < 5; j++) {
+                    let childNodeQuadStripe = new fCore.Node("childNodeQuadStripe");
+                    let randomMaterial = new fCore.Material("Green", fCore.ShaderUniColor, new fCore.CoatColored(new fCore.Color(Math.random(), Math.random(), Math.random(), 1)));
+                    let cmpMaterialQuad = new fCore.ComponentMaterial(randomMaterial);
+                    childNodeQuadStripe.addComponent(new fCore.ComponentMesh(quads));
+                    childNodeQuadStripe.addComponent(new fCore.ComponentTransform());
+                    childNodeQuadStripe.getComponent(fCore.ComponentMesh).mtxPivot.scale(new fCore.Vector3(0.2, 0.2, 0.2));
+                    childNodeQuadStripe.mtxLocal.translateX(i / 10);
+                    childNodeQuadStripe.mtxLocal.translateY(j / 10);
+                    childNodeQuadStripe.addComponent(cmpMaterialQuad);
+                    this.appendChild(childNodeQuadStripe);
+                }
+            }
             this.addComponent(new fCore.ComponentTransform());
-            this.addComponent(cmpMaterialQuad);
             this.mtxLocal.scale(new fCore.Vector3(0.2, 0.2, 0.2));
             this.mtxLocal.translateY(-2);
             this.mtxLocal.translateX(-10);
--- SpaceInvaders/Protection.ts
+++ SpaceInvaders/Protection.ts
@@ -4,12 +4,26 @@ namespace SpaceInvaders {
         constructor(_x: number) {
             super("Protection" + _x);
             let quads: fCore.Mesh = new fCore.MeshQuad("Quads");
-            let cmpMaterialQuad: fCore.ComponentMaterial = new fCore.ComponentMaterial(materialGreen);
 
-            this.addComponent(new fCore.ComponentMesh(quads));
+
+            for (let i: number = 0; i < 5; i++) {
+                for (let j: number = 0; j < 5; j++) {
+                    let childNodeQuadStripe: fCore.Node = new fCore.Node("childNodeQuadStripe");
+                    let randomMaterial: fCore.Material = new fCore.Material("Green", fCore.ShaderUniColor, new fCore.CoatColored(new fCore.Color(Math.random(), Math.random(), Math.random(), 1)));
+                    let cmpMaterialQuad: fCore.ComponentMaterial = new fCore.ComponentMaterial(randomMaterial);
+                    childNodeQuadStripe.addComponent(new fCore.ComponentMesh(quads));
+                    childNodeQuadStripe.addComponent(new fCore.ComponentTransform());
+
+                    childNodeQuadStripe.getComponent(fCore.ComponentMesh).mtxPivot.scale(new fCore.Vector3(0.2, 0.2, 0.2));
+                    childNodeQuadStripe.mtxLocal.translateX(i / 10);
+                    childNodeQuadStripe.mtxLocal.translateY(j / 10);
+
+
+                    childNodeQuadStripe.addComponent(cmpMaterialQuad);
+                    this.appendChild(childNodeQuadStripe);
+                }
+            }
             this.addComponent(new fCore.ComponentTransform());
-            this.addComponent(cmpMaterialQuad);
-
             this.mtxLocal.scale(new fCore.Vector3(0.2, 0.2, 0.2));
             this.mtxLocal.translateY(-2);
             this.mtxLocal.translateX(-10);
