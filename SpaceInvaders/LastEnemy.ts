ee1702993a18d73bd3ccd5bb551597f9967e53f2
vale-sch <vali.vogt@live.de>
2021-05-11 17:17:27 +0200

    SpaceInvaders 2. Ordner gelöscht

--- SpaceInvaders/LastEnemy.ts
+++
@@ -1,9 +0,0 @@
-namespace SpaceInvaders {
-    import fCore = FudgeCore;
-    export class LastEnemy extends QuadNode {
-
-        constructor() {
-            super("LastEnemy", new fCore.Vector2(0, 0.8), new fCore.Vector2(0.15, 0.15));
-        }
-    }
-}
--- SpaceInvaders/ProtectionsStripes.ts
+++
@@ -1,9 +0,0 @@
-namespace SpaceInvaders {
-    import fCore = FudgeCore;
-    export class ProtectionsStripes extends QuadNode {
-        constructor(_name: string, _pos: fCore.Vector2) {
-            super("ProtectionStripe", new fCore.Vector2(_pos.x, _pos.y), new fCore.Vector2(0.05, 0.05));
-        }
-    }
-}
-
